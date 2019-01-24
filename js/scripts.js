1bd0155c57b4cbf31ebd04e00d961a6538e7ae61
Justin Haupt and Tracy Jo Reith <justin@>
2019-01-24 12:03:37 -0800

    support multiple word translations and punctuation at end of words.

--- js/scripts.js
+++ js/scripts.js
@@ -2,31 +2,72 @@
 
 
 const vowels = ["a", "e", "i", "o", "u"];
+const punctuation = [".", ",", ";", "!", "?"];
 
+function translateWord(word) {
+
+  var translation = "";
+
+  // Check for punctuation
+  var punc = null;
+  var lastChar = word.charAt(word.length-1);
+  if(punctuation.includes(lastChar)) {
+    word = word.slice(0, -1);
+    punc = lastChar;
+  }
+
+  var arrText = word.split("");  // split word into array
+
+
+  // first character is a vowel
+  if(vowels.includes(arrText[0])) {
+     translation = word.concat("way");
+  }
+
+  // begin with 'qu'
+  else if (arrText[0] === "q" && arrText[1] === "u") {
+    var newText = arrText.join("");
+    translation = newText.slice(2) + "quay";
+  }
+
+  // begin with consonant + 'qu'
+  else if (!vowels.includes(arrText[0]) && arrText[1] === "q" && arrText[2] === "u") {
+    var newText = arrText.join("");
+    translation =  newText.slice(3) + (arrText[0]) + "quay";
+  }
+
+  // begin with a consonant
+  else {
+    let vowelIndex = 0;
+    for(let i=0; i<arrText.length; ++i) {
+      if(vowels.includes(arrText[i])) {
+        vowelIndex = i;
+        break;
+      }
+    }
+    let newText = arrText.join("");
+    translation = newText.slice(vowelIndex) + newText.slice(0, vowelIndex) + "ay";
+  }
+
+  if(punc !== null) {
+    return translation + punc;
+  }
+
+  return translation;
+}
 
 
 /**
- * Identifies a leap year. Returns a boolean.
+ * Translates a string into pig Latin
  * @param {string} text
  */
 function translate(text) {
-  var translation = "";
 
-  var arrWord = text.toLowerCase().split("");  // split word into array
-
-  //first character is a vowel
-  if(vowels.includes(arrWord[0])) {
-     return text.concat("way");
-  } else if (arrWord[0] === "q" && arrWord[1] === "u") {
-    var newText = arrWord.join("");
-    return newText.slice(2) + "quay";
-  } else if (!vowels.includes(arrWord[0]) && arrWord[1] === "q" && arrWord[2] === "u") {
-    var newText = arrWord.join("");
-    return newText.slice(3) + (arrWord[0]) + "quay";
-  } else {
-    return "not valid text!";
-  }
-
+  var words = text.toLowerCase().split(" ");
+  words = words.map(function(word) {
+      return translateWord(word);
+  });
+  return words.join(" ");
 }
 
 
